4891977996e971cf05953f9a4c482990eae05892
jerryjohns <jerry.johns@student.moringaschool.com>
2022-07-28 16:29:53 +0300

    all

--- src/Contact.js
+++ src/Contact.js
@@ -6,7 +6,21 @@ export default function Contact()
 
   return(
     <div>
-      <p>07977980896785</p>
+      <p> Contact us through
+        <li>Mobile number:
+        <ol>0707629420</ol>
+        </li>
+        <li>Email us:
+          <ol>meyu254@gmail.com</ol>
+        </li>
+        <li> Whatsapp us :
+          <ol>0111677510</ol>
+
+        </li>
+
+        Remember we have got you covered
+      </p>
+      
     </div>
 
   )
